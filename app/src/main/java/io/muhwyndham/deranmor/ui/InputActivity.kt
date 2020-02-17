7fc7c80e7d7c867fdf120dc18dff3d1bbfbbff61
wyndh <wyndham.haryata,p@mail.ugm.ac.id>
2020-02-18 05:46:03 +0700

    dumbing down the input view, moving logic to the view model

--- app/src/main/java/io/muhwyndham/deranmor/ui/InputActivity.kt
+++ app/src/main/java/io/muhwyndham/deranmor/ui/InputActivity.kt
@@ -2,12 +2,15 @@
 
 package io.muhwyndham.deranmor.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import io.muhwyndham.deranmor.R
 import io.muhwyndham.deranmor.model.Report
+import io.muhwyndham.deranmor.viewmodel.CarModelViewModel
 import io.muhwyndham.deranmor.viewmodel.ReportViewModel
 import kotlinx.android.synthetic.main.activity_input.*
 
@@ -15,57 +18,50 @@ class InputActivity : AppCompatActivity() {
 
 
     private var reportViewModel: ReportViewModel? = null
+    private var carModelViewModel: CarModelViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input)
 
         reportViewModel = ViewModelProviders.of(this).get(ReportViewModel::class.java)
+        carModelViewModel = ViewModelProviders.of(this).get(CarModelViewModel::class.java)
+
+        prepareDialogObserver()
+
+        et_tipe_kendaraan.setOnClickListener {
+            startActivity(Intent(this@InputActivity, SearchCarModelActivity::class.java))
+        }
 
         bt_input.setOnClickListener {
-            if (
-                et_nama.text.toString().trim { it <= ' ' }.isNotEmpty()
-                && et_nama.text.toString().trim { it <= ' ' }.isNotEmpty()
-                && et_tipe_kendaraan.text.toString().trim { it <= ' ' }.isNotEmpty()
-                && et_nomor_aduan.text.toString().trim { it <= ' ' }.isNotEmpty()
-
-            ) {
-                val report = Report(
-                    0,
-                    et_nama.text.toString(),
-                    et_nopol.text.toString(),
-                    et_tipe_kendaraan.text.toString(),
-                    if (et_nomor_rangka.text.toString().trim { it <= ' ' }.isNotEmpty()) et_nomor_rangka.text.toString() else "-",
-                    if (et_nomor_mesin.text.toString().trim { it <= ' ' }.isNotEmpty()) et_nomor_mesin.text.toString() else "-",
-                    " ",
-                    et_nomor_aduan.text.toString()
-                )
-
-                reportViewModel?.setReport(report)
-
-                et_nama.setText("")
-                et_nopol.setText("")
-                et_tipe_kendaraan.setText("")
-                et_nomor_rangka.setText("")
-                et_nomor_mesin.setText("")
-                et_nomor_aduan.setText("")
-
-                val toast = Toast.makeText(
-                    this@InputActivity,
-                    "Data telah diinput!",
-                    Toast.LENGTH_LONG
-                )
-                toast.show()
-
-                finish()
-            } else {
-                val toast = Toast.makeText(
-                    this@InputActivity,
-                    "Kolom tidak boleh kosong!",
-                    Toast.LENGTH_LONG
-                )
-                toast.show()
-            }
+            reportViewModel?.validateReportInput(
+                et_nama,
+                et_tipe_kendaraan,
+                et_nomor_aduan
+            )
+                ?.observe(this@InputActivity, Observer {
+                    reportViewModel?.setReport(
+                        it, Report(
+                            0,
+                            et_nama.text.toString(),
+                            et_nopol.text.toString(),
+                            et_tipe_kendaraan.text.toString(),
+                            if (et_nomor_rangka.text.toString().trim { it <= ' ' }.isNotEmpty()) et_nomor_rangka.text.toString() else "-",
+                            if (et_nomor_mesin.text.toString().trim { it <= ' ' }.isNotEmpty()) et_nomor_mesin.text.toString() else "-",
+                            " ",
+                            et_nomor_aduan.text.toString()
+                        )
+                    )
+                })
         }
+    }
+
+    private fun prepareDialogObserver() {
+
+        //toast observer
+        reportViewModel?.toastLiveData?.observe(this@InputActivity, Observer {
+            val toast = Toast.makeText(this@InputActivity, it, Toast.LENGTH_LONG)
+            toast.show()
+        })
     }
 }
--- app/src/main/java/io/muhwyndham/deranmor/viewmodel/ReportViewModel.kt
+++ app/src/main/java/io/muhwyndham/deranmor/viewmodel/ReportViewModel.kt
@@ -1,7 +1,10 @@
 package io.muhwyndham.deranmor.viewmodel
 
 import android.app.Application
+import android.widget.EditText
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.muhwyndham.deranmor.model.Report
 import io.muhwyndham.deranmor.repository.AppRepository
 
@@ -9,11 +12,38 @@ class ReportViewModel(application: Application) : AndroidViewModel(application) 
 
     private var repository: AppRepository = AppRepository(application)
 
+    val toastLiveData: MutableLiveData<String> = MutableLiveData()
+
     fun getReport() = repository.getAllReport()
 
     fun getReportThatContainString(string: String) = repository.getReportThatContainString(string)
 
-    fun setReport(report: Report) {
-        repository.setReport(report)
+    fun setReport(isValid: Boolean, report: Report) {
+
+        if (isValid) repository.setReport(report)
     }
+
+    fun validateReportInput(
+        et_nama: EditText,
+        et_tipe_kendaraan: EditText,
+        et_nomor_aduan: EditText
+    ): LiveData<Boolean> {
+        return if (
+            et_nama.text.toString().trim { it <= ' ' }.isNotEmpty()
+            && et_nama.text.toString().trim { it <= ' ' }.isNotEmpty()
+            && et_tipe_kendaraan.text.toString().trim { it <= ' ' }.isNotEmpty()
+            && et_nomor_aduan.text.toString().trim { it <= ' ' }.isNotEmpty()
+
+        ) {
+            toastLiveData.value = "Data telah diinput!"
+
+            MutableLiveData(true)
+
+        } else {
+            toastLiveData.value = "Kolom tidak boleh kosong!"
+            MutableLiveData(false)
+        }
+    }
+
+
 }
