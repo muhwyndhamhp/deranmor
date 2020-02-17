550ca2f271112ff9785eb79412a3fa8c4713d35f
wyndh <wyndham.haryata,p@mail.ugm.ac.id>
2020-02-18 05:46:24 +0700

    creating activity to search car models

---
+++ app/src/main/java/io/muhwyndham/deranmor/ui/SearchCarModelActivity.kt
@@ -0,0 +1,13 @@
+package io.muhwyndham.deranmor.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.muhwyndham.deranmor.R
+
+class SearchCarModelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_car_model)
+    }
+}
---
+++ app/src/main/java/io/muhwyndham/deranmor/viewmodel/CarModelViewModel.kt
@@ -0,0 +1,9 @@
+package io.muhwyndham.deranmor.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class CarModelViewModel(application: Application) : AndroidViewModel(application) {
+
+
+}
