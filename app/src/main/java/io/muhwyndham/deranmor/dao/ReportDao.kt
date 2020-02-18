a45c0db9c256d7230683d9b8f6ab41f99782098c
wyndh <wyndham.haryata,p@mail.ugm.ac.id>
2020-02-18 21:25:23 +0700

    adding report update function to DAO

--- app/src/main/java/io/muhwyndham/deranmor/dao/ReportDao.kt
+++ app/src/main/java/io/muhwyndham/deranmor/dao/ReportDao.kt
@@ -10,13 +10,16 @@ interface ReportDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun setReport(report: Report)
 
-    @Query("SELECT * FROM report_table ORDER BY id ASC")
+    @Update
+    fun updateReport(report: Report)
+
+    @Query("SELECT * FROM report_table ORDER BY id_nopol ASC")
     fun getAllReport(): LiveData<List<Report>>
 
-    @Query("SELECT * FROM report_table WHERE id = :id")
-    fun getReport(id: Int): LiveData<Report>
+    @Query("SELECT * FROM report_table WHERE id_nopol = :id")
+    fun getReport(id: String): LiveData<Report>
 
-    @Query("SELECT * FROM report_table WHERE (name LIKE :string OR nopol LIKE :string OR tipe_kendaraan LIKE :string OR nomor_rangka LIKE :string OR nomor_mesin LIKE :string OR ovd LIKE :string OR nomor_aduan LIKE :string) ORDER BY name ASC")
+    @Query("SELECT * FROM report_table WHERE (name LIKE :string OR id_nopol LIKE :string OR tipe_kendaraan LIKE :string OR nomor_rangka LIKE :string OR nomor_mesin LIKE :string OR ovd LIKE :string OR nomor_aduan LIKE :string) ORDER BY name ASC")
     fun getReportThatContains(string: String): LiveData<List<Report>>
 
     @Query("DELETE FROM report_table")
