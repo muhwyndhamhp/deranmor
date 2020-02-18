91dc1a9c9379b96387a70b3b51066322225fe25d
wyndh <wyndham.haryata,p@mail.ugm.ac.id>
2020-02-18 10:31:09 +0700

    adding RV and search function for car models, adding function to return result in selection of car model

--- app/src/main/java/io/muhwyndham/deranmor/ui/SearchCarModelActivity.kt
+++ app/src/main/java/io/muhwyndham/deranmor/ui/SearchCarModelActivity.kt
@@ -1,13 +1,78 @@
 package io.muhwyndham.deranmor.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.muhwyndham.deranmor.R
+import io.muhwyndham.deranmor.adapter.SearchCarModelAdapter
+import io.muhwyndham.deranmor.model.CarModel
+import io.muhwyndham.deranmor.viewmodel.CarModelViewModel
+import io.muhwyndham.deranmor.viewmodel.ReportViewModel
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchCarModelActivity : AppCompatActivity() {
+
+    private var carModelViewModel: CarModelViewModel? = null
+
+    private var adapter: SearchCarModelAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_car_model)
+
+        carModelViewModel = ViewModelProviders.of(this).get(CarModelViewModel::class.java)
+
+        carModelViewModel?.getCarModel()
+            ?.observe(this, Observer<List<CarModel>> { this.renderReports(it) })
+
+        et_search.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (et_search.text.toString().trim { it <= ' ' }.isNotEmpty())
+                    carModelViewModel?.getReportThatContainString("%${et_search.text.toString().trim()}%")?.observe(
+                        this@SearchCarModelActivity,
+                        Observer<List<CarModel>> { this@SearchCarModelActivity.renderReports(it) })
+                else if (et_search.text.toString().trim().isEmpty())
+                    carModelViewModel?.getCarModel()?.observe(
+                        this@SearchCarModelActivity,
+                        Observer<List<CarModel>> { this@SearchCarModelActivity.renderReports(it) })
+            }
+
+        })
+
+        bt_search.setOnClickListener {
+            if (et_search.text.toString().trim { it <= ' ' }.isNotEmpty())
+                carModelViewModel?.getReportThatContainString("%${et_search.text.toString().trim()}%")?.observe(
+                    this,
+                    Observer<List<CarModel>> { this.renderReports(it) })
+            else if (et_search.text.toString().trim().isEmpty())
+                carModelViewModel?.getCarModel()?.observe(
+                    this,
+                    Observer<List<CarModel>> { this.renderReports(it) })
+        }
+    }
+
+    fun setCarModel(string: String) {
+        val data = Intent()
+        data.data = Uri.parse(string)
+        setResult(Activity.RESULT_OK, data)
+        finish()
+    }
+
+    private fun renderReports(carModelList: List<CarModel>) {
+        adapter = SearchCarModelAdapter(carModelList, this@SearchCarModelActivity)
+        val layoutManager = LinearLayoutManager(this@SearchCarModelActivity)
+        rv_report.layoutManager = layoutManager
+        rv_report.adapter = adapter
     }
 }
--- app/src/main/java/io/muhwyndham/deranmor/viewmodel/CarModelViewModel.kt
+++ app/src/main/java/io/muhwyndham/deranmor/viewmodel/CarModelViewModel.kt
@@ -2,8 +2,19 @@ package io.muhwyndham.deranmor.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import io.muhwyndham.deranmor.model.CarModel
+import io.muhwyndham.deranmor.repository.AppRepository
 
 class CarModelViewModel(application: Application) : AndroidViewModel(application) {
 
 
+    private var repository: AppRepository = AppRepository(application)
+
+    fun getCarModel() = repository.getAllCarModel()
+
+    fun getReportThatContainString(string: String) = repository.getCarModelThatContainString(string)
+
+    fun setReport(carModel: CarModel) {
+        repository.setCarModel(carModel)
+    }
 }
