198eb2372ef98f39317a4109d4d748e12fa9f15e
wyndh <wyndham.haryata,p@mail.ugm.ac.id>
2020-02-18 21:26:59 +0700

    changing ID from a unique integer id to vehicle register number

--- app/src/main/java/io/muhwyndham/deranmor/model/Report.kt
+++ app/src/main/java/io/muhwyndham/deranmor/model/Report.kt
@@ -7,14 +7,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "report_table")
 data class Report(
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id") var id: Int,
-
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id_nopol") var idNopol: String,
     @ColumnInfo(name = "name") var name: String,
-    @ColumnInfo(name = "nopol") var nopol: String,
     @ColumnInfo(name = "tipe_kendaraan") var tipeKendaraan: String = "minibus",
     @ColumnInfo(name = "nomor_rangka") var nomorRangka: String = "12345678",
     @ColumnInfo(name = "nomor_mesin") var nomorMesin: String = "12345678",
     @ColumnInfo(name = "ovd") var ovd: String = "12345678",
-    @ColumnInfo(name = "nomor_aduan") var nomorAduan: String = "12345678"
+    @ColumnInfo(name = "nomor_aduan") var nomorAduan: String = "12345678",
+    @ColumnInfo(name = "status_aduan") var statusAduan: String = "hilang"
 )
